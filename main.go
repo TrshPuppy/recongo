98e2d153638b315e9b8e53170b8cc24eb32c2bfc
Trash <trshpuppy@proton.me>
2023-07-06 23:21:32 +0000

    main is working.

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	//"os"
+	//"strings"
+)
+
+// const (
+// 	usage = `
+// 	what <option> [--printError
+	
+// 	options:
+//   --help          : display usage information
+//   --version       : display the current version
+//   --printError    : indicates whether error messages are printed to stdout.
+
+//   --cpus          : Return number of CPU cores
+//   --cpu-arch      : Return cpu architecture (e.g. arm64, amd64)
+//   --cpu-cache     : Return the L1,L2,L3 CPU cache size in KB (e.g. 32:128:256 for 32 L1, 128 L2 and 256 L3)
+//   --cpu-info      : Return CPU specifications
+//   --os            : Return the operating system (e.g. darwin, linux, windows)
+//   --os-family     : Return an operating system family (e.g. Windows 10)
+//   --os-version    : Return the operating system version
+//   --ram           : Return the amount of memory (in KB)qq
+//   --software-list : Return a list of software installed on the system`
+// )
+
+func main(){
+	var t string = "tiddies"
+	fmt.Printf("Hello %s!\n", t)
+}
